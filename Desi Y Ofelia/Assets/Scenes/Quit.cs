914725be1ad13b197ff112099a51df465f845c79
Stella Lee <stjilee@adgpm.soe.ucsc.edu>
2016-01-11 18:11:01 -0800

    UI button menu

---
+++ Desi Y Ofelia/Assets/Scenes/Quit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour {
+
+    public void EndGame()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Button pressed!");
+                Application.Quit();
+            }
+        }
+}
