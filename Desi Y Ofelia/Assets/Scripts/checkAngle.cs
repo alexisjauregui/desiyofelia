a5030fbd968060d2eec70f3b294334e308fb3678
Alexis Jauregui <ajaureg2@adgpm.soe.ucsc.edu>
2016-01-18 17:58:37 -0800

    Start vision

---
+++ Desi Y Ofelia/Assets/Scripts/checkAngle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkAngle : MonoBehaviour {
+
+    public float fieldOV = 110f;
+    public bool playerIS;
+    private GameObject Cube;
+    
+
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject == Cube)
+        {
+
+        }
+    }
+}
