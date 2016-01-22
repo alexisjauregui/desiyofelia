5c14cf0faf2714db4f4753f2a332e83aac7215dc
unknown <ajaureg2@Mario.adgpm.soe.ucsc.edu>
2016-01-21 20:26:16 -0800

    Feilds of vision

---
+++ Desi Y Ofelia/Assets/Scripts/FieldOfVision.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldOfVision : MonoBehaviour {
+
+    public float viewRadius;
+    [Range (0,360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstaclesMask;
+
+    public List<Transform> visibleTarget = new List<Transform>();
+
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+    }
+
+
+    IEnumerable FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisTarget();
+        }
+    }
+
+    void FindVisTarget()
+    {
+        visibleTarget.Clear();
+        Collider[] targetVisibleRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for(int i = 0; i <targetVisibleRadius.Length; i++)
+        {
+            Transform target = targetVisibleRadius[i].transform;
+            Vector3 dirtoTarget = (target.position - transform.position).normalized;
+            if(Vector3.Angle(transform.forward,dirtoTarget)< viewAngle / 2)
+            {
+                float dsttoTarget = Vector3.Distance(transform.position, target.position);
+
+                if(!Physics.Raycast(transform.position, dirtoTarget, dsttoTarget, obstaclesMask))
+                {
+                    visibleTarget.Add(target);
+                }
+            }
+        }
+    }
+
+
+	// Update is called once per frame
+	public Vector3 DirFromAngle(float angleinDeg, bool angleisGlobal)
+    {
+        if (!angleisGlobal)
+        {
+            angleinDeg += transform.eulerAngles.y;
+        
+        }
+        return new Vector3(Mathf.Sin(angleinDeg * Mathf.Deg2Rad), 0, Mathf.Cos(angleinDeg * Mathf.Deg2Rad));
+    }
+}
