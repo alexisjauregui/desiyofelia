170fdbbeeef0e68750b3a0d14f3414cf1c2fcd4a
unknown <ajaureg2@Mario.adgpm.soe.ucsc.edu>
2016-01-25 17:48:54 -0800

    Cage Unlock

---
+++ Desi Y Ofelia/Assets/Scripts/cageSwitch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class cageSwitch : MonoBehaviour {
+
+    private string passCode;
+    public GameObject Cage;
+    public Text text;
+	
+    void Start()
+    {
+        passCode = "";
+       
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        text.text = passCode;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            passCode += "Z";
+            Debug.Log(passCode.Length);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            passCode += "X";
+        }
+
+        if(passCode == "XZXZ")
+        {
+            Destroy(Cage, 0);
+        }
+
+        if( passCode.Length == 5)
+        {
+            passCode = "";
+        }
+
+	}
+}
