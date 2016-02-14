f69e44b8d3ab1479f60a8a9494f4b78060a6120e
MiniWitch3 <med94man@yahoo.com>
2016-02-13 21:10:44 -0800

    Adding a bit into Scene.cs and the Archway
    Archway is to still be improved on.

--- Desi Y Ofelia/Assets/Scripts/Scene.cs
+++ Desi Y Ofelia/Assets/Scripts/Scene.cs
@@ -23,7 +23,7 @@ public class Scene : MonoBehaviour {
             {
                 Debug.Log("Lobby Opened");
                 SceneManager.LoadScene("Level Lobby");
-                //SceneManager.MoveGameObjectToScene(Desi, 2);
+                //SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
             }
         }else if(SceneManager.GetActiveScene().name == "Level Lobby")
         {
@@ -31,23 +31,79 @@ public class Scene : MonoBehaviour {
             {
                 Debug.Log("Level 0 Opened");
                 SceneManager.LoadScene("Level 0");
-                //SceneManager.MoveGameObjectToScene(Desi, 0);
+                //SceneManager.MoveGameObjectToScene(Desi, Level 0);
             }
             else if(Desi.position.x >= 10.25 && Desi.position.z > 48.64 && Desi.position.z < 53.85)
             {
                 Debug.Log("Level 1 Opened");
                 SceneManager.LoadScene("Level 1");
-                //SceneManager.MoveGameObjectToScene(Desi, 1);
+                //SceneManager.MoveGameObjectToScene(Desi, Level 1);
             }
+			else if (Desi.position.x <= -8 && Desi.position.z > 75 && Desi.position.z < 80)
+			{
+				Debug.Log ("Level 2 Opened");
+				//SceneManager.LoadScene ("Level 2");
+				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 2");
+			}
+			else if (Desi.position.x >= 10 && Desi.position.z > 97 && Desi.position.z < 103)
+			{
+				Debug.Log ("Level 3 Opened");
+				//SceneManager.LoadScene ("Level 3");
+				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 3");
+			}
+			else if (Desi.position.z <= -8 && Desi.position.z > 120 && Desi.position.z < 125)
+			{
+				Debug.Log ("Level 4 Opened");
+				//SceneManager.LoadScene ("Level 4");
+				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 4");
+			}
+			else if (Desi.position.x >= 10 && Desi.position.z > 157 && Desi.position.z < 162)
+			{
+				Debug.Log ("Level 5 Opened");
+				//SceneManager.LoadScene ("Level 5");
+				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 5");
+			}
         }else if(SceneManager.GetActiveScene().name == "Level 1")
         {
-            if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38)
+			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
             {
                 Debug.Log("Lobby Opened");
                 SceneManager.LoadScene("Level Lobby");
-                //SceneManager.MoveGameObjectToScene(Desi, 2);
+                //SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
             }
-        }
+		}/*else if(SceneManager.GetActiveScene().name == "Level 2")
+		{
+			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			{
+				Debug.Log("Lobby Opened");
+				SceneManager.LoadScene("Level Lobby");
+				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+			}
+		}else if(SceneManager.GetActiveScene().name == "Level 3")
+		{
+			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			{
+				Debug.Log("Lobby Opened");
+				SceneManager.LoadScene("Level Lobby");
+				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+			}
+		}else if(SceneManager.GetActiveScene().name == "Level 4")
+		{
+			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			{
+				Debug.Log("Lobby Opened");
+				SceneManager.LoadScene("Level Lobby");
+				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+			}
+		}else if(SceneManager.GetActiveScene().name == "Level 5")
+		{
+			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			{
+				Debug.Log("Lobby Opened");
+				SceneManager.LoadScene("Level Lobby");
+				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+			}
+		}*/
 
     }
 
