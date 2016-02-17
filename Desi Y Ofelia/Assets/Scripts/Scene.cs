dee537f5c1d5dbd5a7e58f6173850f04b3ae94fd
MiniWitch3 <med94man@yahoo.com>
2016-02-16 21:25:07 -0800

    Ceilings Added and Faces Fixed

--- Desi Y Ofelia/Assets/Scripts/Scene.cs
+++ Desi Y Ofelia/Assets/Scripts/Scene.cs
@@ -11,6 +11,10 @@ public class Scene : MonoBehaviour {
     private Vector3 curr_position;
     private bool doorCollision0;
     private bool doorCollision1;
+	private bool doorCollision2;
+	private bool doorCollision3;
+	private bool doorCollision4;
+	private bool doorCollision5;
 
 	// Use this for initialization
 	void Start () {
@@ -44,29 +48,25 @@ public class Scene : MonoBehaviour {
                 Debug.Log("Level 1 Opened");
                 NetworkManager.singleton.ServerChangeScene("Level 1");
             }
-			else if (Desi.position.x <= -8 && Desi.position.z > 75 && Desi.position.z < 80)
+			else if (doorCollision2)
 			{
 				Debug.Log ("Level 2 Opened");
-				//SceneManager.LoadScene ("Level 2");
-				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 2");
+				//NetworkManager.singleton.ServerChangeScene("Level 2");
 			}
-			else if (Desi.position.x >= 10 && Desi.position.z > 97 && Desi.position.z < 103)
+			else if (doorCollision3)
 			{
 				Debug.Log ("Level 3 Opened");
-				//SceneManager.LoadScene ("Level 3");
-				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 3");
+				//NetworkManager.singleton.ServerChangeScene("Level 3");
 			}
-			else if (Desi.position.z <= -8 && Desi.position.z > 120 && Desi.position.z < 125)
+			else if (doorCollision4)
 			{
 				Debug.Log ("Level 4 Opened");
-				//SceneManager.LoadScene ("Level 4");
-				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 4");
+				//NetworkManager.singleton.ServerChangeScene("Level 4");
 			}
-			else if (Desi.position.x >= 10 && Desi.position.z > 157 && Desi.position.z < 162)
+			else if (doorCollision5)
 			{
 				Debug.Log ("Level 5 Opened");
-				//SceneManager.LoadScene ("Level 5");
-				//SceneManager.MoveGameObjectToScene(Desi_obj, "Level 5");
+				//NetworkManager.singleton.ServerChangeScene("Level 5");
 			}
         }else if(SceneManager.GetActiveScene().name == "Level 1")
         {
@@ -81,35 +81,47 @@ public class Scene : MonoBehaviour {
             }
         }/*else if(SceneManager.GetActiveScene().name == "Level 2")
 		{
-			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			if (HasCandle())
 			{
-				Debug.Log("Lobby Opened");
-				SceneManager.LoadScene("Level Lobby");
-				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+				Debug.Log("You are holding a candle");
+				if (doorCollision2)
+				{
+					Debug.Log("Lobby Opened");
+					//NetworkManager.singleton.ServerChangeScene("Level Lobby");
+				}
 			}
 		}else if(SceneManager.GetActiveScene().name == "Level 3")
 		{
-			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			if (HasCandle())
 			{
-				Debug.Log("Lobby Opened");
-				SceneManager.LoadScene("Level Lobby");
-				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+				Debug.Log("You are holding a candle");
+                if (doorCollision3)
+                {
+					Debug.Log("Lobby Opened");
+					//NetworkManager.singleton.ServerChangeScene("Level Lobby");
+				}
 			}
 		}else if(SceneManager.GetActiveScene().name == "Level 4")
 		{
-			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			if (HasCandle())
 			{
-				Debug.Log("Lobby Opened");
-				SceneManager.LoadScene("Level Lobby");
-				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+				Debug.Log("You are holding a candle");
+				if(doorCollision4)
+				{
+					Debug.Log("Lobby Opened");
+					//NetworkManager.singleton.ServerChangeScene("Level Lobby");
+				}
 			}
 		}else if(SceneManager.GetActiveScene().name == "Level 5")
 		{
-			if (Desi.position.x >= 1.11 && Desi.position.z > -2.29 && Desi.position.z < 4.38 && HasCandle())
+			if (HasCandle())
 			{
-				Debug.Log("Lobby Opened");
-				SceneManager.LoadScene("Level Lobby");
-				//SceneManager.MoveGameObjectToScene(Desi, Level Lobby);
+				Debug.Log("You are holding a candle");
+				if(doorCollision5)
+				{
+					Debug.Log("Lobby Opened");
+					//NetworkManager.singleton.ServerChangeScene("Level Lobby");
+				}
 			}
 		}*/
 
@@ -121,6 +133,14 @@ public class Scene : MonoBehaviour {
             doorCollision0 = true;
         if (door.gameObject.tag == "Door1")
             doorCollision1 = true; 
+		if (door.gameObject.tag == "Door2")
+			doorCollision2 = true; 
+		if (door.gameObject.tag == "Door3")
+			doorCollision3 = true; 
+		if (door.gameObject.tag == "Door4")
+			doorCollision4 = true; 
+		if (door.gameObject.tag == "Door5")
+			doorCollision5 = true; 
     }
 
     void OnTriggerExit(Collider door)
@@ -129,6 +149,14 @@ public class Scene : MonoBehaviour {
             doorCollision0 = false;
         if (door.gameObject.tag == "Door1")
             doorCollision1 = false;
+		if (door.gameObject.tag == "Door2")
+			doorCollision2 = false;
+		if (door.gameObject.tag == "Door3")
+			doorCollision3 = false;
+		if (door.gameObject.tag == "Door4")
+			doorCollision4 = false;
+		if (door.gameObject.tag == "Door5")
+			doorCollision5 = false;
     }
 
     bool HasCandle()
