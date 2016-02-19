f9a0c6f0a1685250a4848798eefd6b3e441212e2
unknown <ajaureg2@seamus.adgpm.soe.ucsc.edu>
2016-02-19 15:48:11 -0800

    Update for MM

--- Desi Y Ofelia/Assets/Scripts/openDoor.cs
+++ Desi Y Ofelia/Assets/Scripts/openDoor.cs
@@ -1,36 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking; 
 using System.Collections;
 
-public class openDoor : MonoBehaviour {
+public class openDoor : NetworkBehaviour {
 
     private GameObject Desi;
     private GameObject Key;
 
 	void Start()
     {
-        Desi = GameObject.FindGameObjectWithTag("Desi");
+        Desi = GameObject.FindGameObjectWithTag("DesiPos");
         Key = GameObject.Find("Key");
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+        Desi = GameObject.FindGameObjectWithTag("DesiPos");
 
-        Desi = GameObject.FindGameObjectWithTag("Desi");
-
-        
-
-        if (Vector3.Distance(transform.position, Desi.transform.position)<5)
+        if (Vector3.Distance(this.transform.position, Desi.transform.position)<5)
         {
             
             if (GameObject.Find("Key").transform.IsChildOf(Desi.transform))
             {
-                Destroy(GameObject.Find("ClosetDoor"));
-                Key.transform.parent = null;
-                Destroy(Key); 
+				CmdDestroyDoor ();    
             }
         }
-        
-
-
 	}
+
+	[Command]
+	void CmdDestroyDoor()
+	{
+		Destroy (GameObject.Find ("ClosetDoor"));
+		Key.transform.parent = null;
+		Destroy (Key);
+	}
+
+
 }
