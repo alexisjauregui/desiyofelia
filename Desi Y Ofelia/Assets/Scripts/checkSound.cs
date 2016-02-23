10288373a604fc9489c7d06556a0397fa3df2b81
unknown <swong30@CallofDuty2.adgpm.soe.ucsc.edu>
2016-02-22 17:53:34 -0800

    Remade checkchords.cs
    removed excess checkchords scripts and prefab, condensed all into one
    script, now audio sources are added on Awake

--- Desi Y Ofelia/Assets/Scripts/checkSound.cs
+++ Desi Y Ofelia/Assets/Scripts/checkSound.cs
@@ -1,30 +1,62 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using System.Collections;
 
-
-public class checkSound : MonoBehaviour
+[RequireComponent(typeof(AudioSync))]
+public class checkSound : NetworkBehaviour
 {
-  
-    public AudioSource audio;
 
+    public AudioClip clipA;
+    public AudioClip clipB;
+    public AudioClip clipX;
+    public AudioClip clipY;
+
+    private AudioSource audioA;
+    private AudioSource audioB;
+    private AudioSource audioX;
+    private AudioSource audioY;
+    private AudioSync audiosyc;
 
     void Start()
     {
-  
-        audio = GetComponent<AudioSource>();
+        audiosyc = this.GetComponent<AudioSync>();
     }
 
+    public AudioSource AddAudio(AudioClip clip, bool playAwake)
+    {
+        AudioSource newAudio = gameObject.AddComponent<AudioSource>();
+        newAudio.clip = clip;
+        newAudio.playOnAwake = playAwake;
+        return newAudio;
+    }
+
+    void Awake()
+    {
+        audioA = AddAudio(clipA, false);
+        audioB = AddAudio(clipB, false);
+        audioX = AddAudio(clipX, false);
+        audioY = AddAudio(clipY, false);
+    }
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (Input.GetButtonDown("AButton"))
         {
- 
-            audio.Play();
+            audiosyc.PlaySound(audioA);
+        }
+        if (Input.GetButtonDown("BButton"))
+        {
+            audiosyc.PlaySound(audioB);
+        }
+        if (Input.GetButtonDown("XButton"))
+        {
+            audiosyc.PlaySound(audioX);
+        }
+        if (Input.GetButtonDown("YButton"))
+        {
+            audiosyc.PlaySound(audioY);
         }
     }
 }
