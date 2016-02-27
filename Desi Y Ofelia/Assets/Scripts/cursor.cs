af3ec66baf4fa1bfd825be45c7e5586d4f8c7db6
Alexis Jauregui <ajaureg2@adgpm.soe.ucsc.edu>
2016-02-27 15:40:06 -0800

    Crosshair switch done

--- Desi Y Ofelia/Assets/Scripts/cursor.cs
+++ Desi Y Ofelia/Assets/Scripts/cursor.cs
@@ -4,25 +4,44 @@ using System.Collections;
 public class cursor : MonoBehaviour {
     private RaycastHit hit;
     private float distance;
+    private GameObject crosshair;
+    public Texture image1;
+    public Texture image2;
+    
 
 	// Use this for initialization
 	void Start ()
     {
         distance = 2;
-	
+        crosshair = GameObject.Find("Crosshair");
+
 	}
 
     // Update is called once per frame
     void Update()
     {
+        crosshair.GetComponent<Renderer>().material.mainTexture = image1;
+
         if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, distance))
         {
-            if(hit.collider.gameObject.transform.GetChild(0).gameObject.tag == "Interact")
+            Debug.Log(hit.collider.name);
+            if (hit.collider.gameObject.transform.childCount > 0)
             {
-                // Write switch canvas
-
+                if (hit.collider.gameObject.transform.GetChild(0).gameObject.tag == "Interact")
+                {
+                    crosshair.GetComponent<Renderer>().material.mainTexture = image2;
+                }
+      
+               
+           
             }
-            
+           
+          
         }
+
+
+       
     }
+
+
 }
