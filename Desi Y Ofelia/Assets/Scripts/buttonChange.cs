7c7fd6e80cc46f02cd639ceceb0690787388bd99
Stella Lee <stjilee@adgpm.soe.ucsc.edu>
2016-02-29 23:06:05 -0800

    working on Ofelia UI

---
+++ Desi Y Ofelia/Assets/Scripts/buttonChange.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+/*
+   
+
+*/
+
+public class buttonChange : MonoBehaviour
+{
+	private Image button;
+
+	void Start()
+	{
+		button = GameObject.Find("Yellow").GetComponent<Image>();
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		button = GetComponent<Image>().sprite;
+
+		if (Input.GetButtonDown("YButton"))
+		{
+			button.GetComponent<Image>().sprite = "buttonYellowP";
+		}
+
+	}
+}
