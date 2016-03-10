45ef9fa4b8324e5708a4001d6b399e644a43af15
unknown <ajaureg2@link.adgpm.soe.ucsc.edu>
2016-03-09 16:46:56 -0800

    Lookat Indicator

---
+++ Desi Y Ofelia/Assets/alwaysface.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class alwaysface : MonoBehaviour {
+    private Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GameObject.FindGameObjectWithTag("DesiPlayer"))
+        {
+            target = GameObject.FindGameObjectWithTag("DesiPlayer").transform;
+        }
+        if(target != null)
+        {
+            this.transform.LookAt(target);
+        }
+        //Debug.Log(target.name);
+	}
+}
