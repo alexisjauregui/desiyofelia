43c97899318c4b2ee43f6b8425afcd7dfdde7111
Alexis Jauregui <ajaureg2@adgpm.soe.ucsc.edu>
2016-03-10 18:18:42 -0800

    Done

--- Desi Y Ofelia/Assets/Scripts/cursor.cs
+++ Desi Y Ofelia/Assets/Scripts/cursor.cs
@@ -5,24 +5,21 @@ public class cursor : MonoBehaviour {
     private RaycastHit hit;
     private float distance;
     private GameObject crosshair;
-    public Texture image1;
-    public Texture image2;
+    public Sprite image2;
     private float scale;
     
 
 	// Use this for initialization
 	void Start ()
     {
-        distance = 15;
+        distance = 2;
         crosshair = GameObject.Find("Crosshair");
-
 	}
 
     // Update is called once per frame
     void Update()
     {
-        crosshair.GetComponent<Renderer>().material.mainTexture = image1;
-
+        crosshair.GetComponent<SpriteRenderer>().sprite = null;
         if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, distance))
         {
             //Debug.Log(hit.collider.name);
@@ -30,30 +27,10 @@ public class cursor : MonoBehaviour {
             {
                 if (hit.collider.gameObject.transform.GetChild(0).gameObject.tag == "Interact")
                 {
-                    //Debug.Log(Vector3.Distance(transform.position,hit.collider.transform.position));
-                    crosshair.GetComponent<Renderer>().material.mainTexture = image2;
-                    crosshair.transform.localScale = new Vector3( xScale(Vector3.Distance(transform.position, hit.collider.transform.position)), xScale(Vector3.Distance(transform.position, hit.collider.transform.position)), 0);
-
+                    crosshair.GetComponent<SpriteRenderer>().sprite = image2;
 
                 }
-      
-               
-           
-            }
-           
-          
+            }         
         }       
     }
-
-    float xScale(float distance)
-    {
-        //Debug.Log(distance);
-        scale = (distance / 20);
-        scale = scale * .48f;
-        scale += 0.025f;
-        scale = 1 - scale;
-        scale -= 0.6f;
-        return scale;
-    }
-
 }
