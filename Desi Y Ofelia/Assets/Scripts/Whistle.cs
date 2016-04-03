aaa6de3d062f7fa5d16e1819de8a536cebaa60c1
unknown <ajaureg2@link.adgpm.soe.ucsc.edu>
2016-04-03 15:10:25 -0700

    Whistle stuff added

---
+++ Desi Y Ofelia/Assets/Scripts/Whistle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Whistle : NetworkBehaviour {
+    public AudioClip clip;
+    private AudioSource source;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        source = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (isLocalPlayer)
+        {
+            if (Input.GetButtonDown("AButton"))
+            {
+                source.Play();
+            }
+        }
+	
+	}
+
+}
