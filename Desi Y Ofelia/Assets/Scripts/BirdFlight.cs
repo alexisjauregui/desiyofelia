c386ed0ed2acdb1f618e5af5ae947f3fdcf4bf4f
unknown <swong30@cmdrkeen.adgpm.soe.ucsc.edu>
2016-04-22 16:42:09 -0700

    Change to the birds in Level 3 so they always face the center.

--- Desi Y Ofelia/Assets/Scripts/BirdFlight.cs
+++ Desi Y Ofelia/Assets/Scripts/BirdFlight.cs
@@ -8,6 +8,7 @@ public class BirdFlight : MonoBehaviour {
     private GameObject Bird3;
     private GameObject Bird4;
     private Vector3 Pos2;
+    private Vector3 rot;
 
 
 
@@ -19,7 +20,7 @@ public class BirdFlight : MonoBehaviour {
         Bird3 = GameObject.Find("Bird3");
         Bird4 = GameObject.Find("Bird4");
        
-
+        rot = new Vector3(Bird1.transform.eulerAngles.x, Bird1.transform.eulerAngles.y, Bird1.transform.eulerAngles.z);
 
     }
 	
@@ -30,18 +31,22 @@ public class BirdFlight : MonoBehaviour {
             if (Idol.transform.eulerAngles.y < 80)
             {
                 Position1();
+                Debug.Log("1");
             }
             if (Idol.transform.eulerAngles.y > 80 && Idol.transform.eulerAngles.y < 95)
             {
                 Position2();
+                Debug.Log("2");
             }
             if (Idol.transform.eulerAngles.y > 170 && Idol.transform.eulerAngles.y < 190)
             {
                 Position3();
+                Debug.Log("3");
             }
            if(Idol.transform.eulerAngles.y == 270)
             {
                 Position4();
+                Debug.Log("4");
             }
            
         }
@@ -55,6 +60,13 @@ public class BirdFlight : MonoBehaviour {
         Bird3.transform.position = new Vector3(-7.75f, 6.25f, -29.5f);
         Bird4.transform.position = new Vector3(-3, 6.25f, -29.5f);
 
+        Vector3 rot4 = new Vector3(rot.x, rot.y + 90, rot.z);
+
+        Bird1.transform.eulerAngles = rot4;
+        Bird2.transform.eulerAngles = rot4;
+        Bird3.transform.eulerAngles = rot4;
+        Bird4.transform.eulerAngles = rot4;
+
     }
     void Position2()
     {
@@ -63,6 +75,12 @@ public class BirdFlight : MonoBehaviour {
         Bird3.transform.position = new Vector3(-30, 6.25f, 2);
         Bird4.transform.position = new Vector3(-30, 6.25f, 7);
 
+        Vector3 rot2 = new Vector3(rot.x, rot.y + 180, rot.z);
+
+        Bird1.transform.eulerAngles = rot2;
+        Bird2.transform.eulerAngles = rot2;
+        Bird3.transform.eulerAngles = rot2;
+        Bird4.transform.eulerAngles = rot2;
     }
     void Position3()
     {
@@ -70,6 +88,13 @@ public class BirdFlight : MonoBehaviour {
         Bird2.transform.position = new Vector3(6.75f, 6.25f, 29);
         Bird3.transform.position = new Vector3(-1.1f, 6.25f, 29);
         Bird4.transform.position = new Vector3(-5.5f, 6.25f, 29);
+
+        Vector3 rot3 = new Vector3(rot.x, rot.y + 270, rot.z);
+
+        Bird1.transform.eulerAngles = rot3;
+        Bird2.transform.eulerAngles = rot3;
+        Bird3.transform.eulerAngles = rot3;
+        Bird4.transform.eulerAngles = rot3;
     }
     void Position4()
     {
@@ -77,6 +102,12 @@ public class BirdFlight : MonoBehaviour {
         Bird2.transform.position = new Vector3(30f, 6.25f, -2.3f);
         Bird3.transform.position = new Vector3(30f, 6.25f, -4.3f);
         Bird4.transform.position = new Vector3(30f, 6.25f, 0.6f);
+
         
+
+        Bird1.transform.eulerAngles = rot;
+        Bird2.transform.eulerAngles = rot;
+        Bird3.transform.eulerAngles = rot;
+        Bird4.transform.eulerAngles = rot;
     }
 }
--- Desi Y Ofelia/Assets/Scripts/levelTimeGate.cs
+++ Desi Y Ofelia/Assets/Scripts/levelTimeGate.cs
@@ -10,6 +10,8 @@ public class levelTimeGate : NetworkBehaviour {
     private string chords;
     private Text newText;
     private GameObject cage;
+    private GameObject gate;
+    private bool gatebool;
 
 
     // Use this for initialization
@@ -19,6 +21,8 @@ public class levelTimeGate : NetworkBehaviour {
             newText = GameObject.FindGameObjectWithTag("Switch").GetComponent<Text>();
             chords = "";
             cage = GameObject.FindGameObjectWithTag("Cage");
+            //gate = GameObject.FindGameObjectWithTag("Gate");  Need Gate Asset
+            gatebool = false;
         }
     }
 
@@ -30,10 +34,15 @@ public class levelTimeGate : NetworkBehaviour {
             newText = GameObject.FindGameObjectWithTag("Switch").GetComponent<Text>();
             chords = newText.text;
 
-            if (chords.Contains("ABXY") && cage.transform.position == new Vector3(15,1,-17))
+            if (chords.Contains("ABXY") && cage.transform.position == new Vector3(15,1,-17)) //Can change chords to any combo
             {
                 cage.GetComponent<Animation>().Play();
-                
+                gatebool = true;
+            }
+
+            if (chords.Contains("YAAX") ) //Must add condition for Gate position vector
+            {
+                //gate.GetComponent<Animation>().Play();
             }
         }
     }
