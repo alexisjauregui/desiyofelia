278253c3c6b4329a0e34469cd89a9bf7883900da
unknown <swong30@cmdrkeen.adgpm.soe.ucsc.edu>
2016-04-22 19:02:02 -0700

    Add code in Level 3 so banners become white after the correct chords are played - Suihong Wong

--- Desi Y Ofelia/Assets/Scripts/BirdFlight.cs
+++ Desi Y Ofelia/Assets/Scripts/BirdFlight.cs
@@ -1,52 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BirdFlight : MonoBehaviour {
+public class BirdFlight : MonoBehaviour
+{
     private GameObject Idol;
     private GameObject Bird1;
     private GameObject Bird2;
     private GameObject Bird3;
     private GameObject Bird4;
     private Vector3 Pos2;
+    private Vector3 rot;
 
 
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         Bird1 = GameObject.Find("Bird1");
         Bird2 = GameObject.Find("Bird2");
         Bird3 = GameObject.Find("Bird3");
         Bird4 = GameObject.Find("Bird4");
-       
 
+        rot = new Vector3(Bird1.transform.eulerAngles.x, Bird1.transform.eulerAngles.y, Bird1.transform.eulerAngles.z);
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if(Idol = GameObject.Find("Idol(Clone)"))
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Idol = GameObject.Find("Idol(Clone)"))
         {
             if (Idol.transform.eulerAngles.y < 80)
             {
                 Position1();
+                Debug.Log("1");
             }
             if (Idol.transform.eulerAngles.y > 80 && Idol.transform.eulerAngles.y < 95)
             {
                 Position2();
+                Debug.Log("2");
             }
             if (Idol.transform.eulerAngles.y > 170 && Idol.transform.eulerAngles.y < 190)
             {
                 Position3();
+                Debug.Log("3");
             }
-           if(Idol.transform.eulerAngles.y == 270)
+            if (Idol.transform.eulerAngles.y == 270)
             {
                 Position4();
+                Debug.Log("4");
             }
-           
+
         }
-	
-	}
+
+    }
 
     void Position1()
     {
@@ -54,6 +61,13 @@ public class BirdFlight : MonoBehaviour {
         Bird2.transform.position = new Vector3(-5.75f, 6.25f, -29.5f);
         Bird3.transform.position = new Vector3(-7.75f, 6.25f, -29.5f);
         Bird4.transform.position = new Vector3(-3, 6.25f, -29.5f);
+
+        Vector3 rot4 = new Vector3(rot.x, rot.y + 90, rot.z);
+
+        Bird1.transform.eulerAngles = rot4;
+        Bird2.transform.eulerAngles = rot4;
+        Bird3.transform.eulerAngles = rot4;
+        Bird4.transform.eulerAngles = rot4;
 
     }
     void Position2()
@@ -63,6 +77,12 @@ public class BirdFlight : MonoBehaviour {
         Bird3.transform.position = new Vector3(-30, 6.25f, 2);
         Bird4.transform.position = new Vector3(-30, 6.25f, 7);
 
+        Vector3 rot2 = new Vector3(rot.x, rot.y + 180, rot.z);
+
+        Bird1.transform.eulerAngles = rot2;
+        Bird2.transform.eulerAngles = rot2;
+        Bird3.transform.eulerAngles = rot2;
+        Bird4.transform.eulerAngles = rot2;
     }
     void Position3()
     {
@@ -70,6 +90,13 @@ public class BirdFlight : MonoBehaviour {
         Bird2.transform.position = new Vector3(6.75f, 6.25f, 29);
         Bird3.transform.position = new Vector3(-1.1f, 6.25f, 29);
         Bird4.transform.position = new Vector3(-5.5f, 6.25f, 29);
+
+        Vector3 rot3 = new Vector3(rot.x, rot.y + 270, rot.z);
+
+        Bird1.transform.eulerAngles = rot3;
+        Bird2.transform.eulerAngles = rot3;
+        Bird3.transform.eulerAngles = rot3;
+        Bird4.transform.eulerAngles = rot3;
     }
     void Position4()
     {
@@ -77,6 +104,12 @@ public class BirdFlight : MonoBehaviour {
         Bird2.transform.position = new Vector3(30f, 6.25f, -2.3f);
         Bird3.transform.position = new Vector3(30f, 6.25f, -4.3f);
         Bird4.transform.position = new Vector3(30f, 6.25f, 0.6f);
-        
+
+
+
+        Bird1.transform.eulerAngles = rot;
+        Bird2.transform.eulerAngles = rot;
+        Bird3.transform.eulerAngles = rot;
+        Bird4.transform.eulerAngles = rot;
     }
 }
--- Desi Y Ofelia/Assets/Scripts/levelTimeGate.cs
+++ Desi Y Ofelia/Assets/Scripts/levelTimeGate.cs
@@ -4,21 +4,27 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
-public class levelTimeGate : NetworkBehaviour {
+public class levelTimeGate : NetworkBehaviour
+{
 
     private string pass;
     private string chords;
     private Text newText;
     private GameObject cage;
+    private GameObject gate;
+    private bool gatebool;
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         if (SceneManager.GetActiveScene().name == "Level 4")
         {
             newText = GameObject.FindGameObjectWithTag("Switch").GetComponent<Text>();
             chords = "";
             cage = GameObject.FindGameObjectWithTag("Cage");
+            //gate = GameObject.FindGameObjectWithTag("Gate");  Need Gate Asset
+            gatebool = false;
         }
     }
 
@@ -30,13 +36,18 @@ public class levelTimeGate : NetworkBehaviour {
             newText = GameObject.FindGameObjectWithTag("Switch").GetComponent<Text>();
             chords = newText.text;
 
-            if (chords.Contains("ABXY") && cage.transform.position == new Vector3(15,1,-17))
+            if (chords.Contains("ABXY") && cage.transform.position == new Vector3(15, 1, -17)) //Can change chords to any combo
             {
                 cage.GetComponent<Animation>().Play();
-                
+                gatebool = true;
+            }
+
+            if (chords.Contains("YAAX")) //Must add condition for Gate position vector
+            {
+                //gate.GetComponent<Animation>().Play();
             }
         }
     }
 
-   
+
 }
