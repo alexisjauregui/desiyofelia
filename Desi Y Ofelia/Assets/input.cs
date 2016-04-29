f63191a57aba8dc61e7e8d993182258af3c8c335
Suihong Wong <swong30@ucsc.edu>
2016-04-28 20:54:32 -0700

    Start Menu push, need testing

---
+++ Desi Y Ofelia/Assets/input.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class input : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+            
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Fire1"))
+        {
+            InputField input = this.GetComponent<InputField>();
+            input.ActivateInputField();
+            input.Select();
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            InputField input = this.GetComponent<InputField>();
+            input.ActivateInputField();
+            input.Select();
+        }
+    }
+}
