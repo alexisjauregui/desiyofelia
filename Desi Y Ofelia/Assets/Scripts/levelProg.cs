41fd19557b4c432d017591ccbd1351158cc2ccd1
Alexis Jauregui <ajaureg2@link.adgpm.soe.ucsc.edu>
2016-05-01 16:32:35 -0700

    testing Tracker

--- Desi Y Ofelia/Assets/Scripts/levelProg.cs
+++ Desi Y Ofelia/Assets/Scripts/levelProg.cs
@@ -1,19 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class levelProg : MonoBehaviour {
     bool level0;
     bool level1;
     bool level2;
-    bool level3;
+    bool level3 = false;
+    string currentLevel;
+    string lastlevel;
 
     void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentLevel != SceneManager.GetActiveScene().name)
+        { 
+            lastlevel = currentLevel;
+        }
+
+        currentLevel = SceneManager.GetActiveScene().name;
+
+        Debug.Log("current: "+currentLevel);
+        Debug.Log("last: " + lastlevel);
+
+
+        if (SceneManager.GetActiveScene().name == "Level Lobby")
+        {
+            Debug.Log("Currently in Lobby");
+            lobbyProg();
+        }
 	
 	}
+
+    void lobbyProg()
+    {
+        if (level3)
+        {
+            Destroy(GameObject.Find("Cover"));
+        }
+
+    }
 }
