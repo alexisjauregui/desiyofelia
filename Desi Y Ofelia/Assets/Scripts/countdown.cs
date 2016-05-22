327c0b3bb26ea628d0cbe20462d383c1e994305f
Alexis Jauregui <ajaureg2@adgpm.soe.ucsc.edu>
2016-05-22 13:33:25 -0700

    UI Timer Code done Placeholder image is there

---
+++ Desi Y Ofelia/Assets/Scripts/countdown.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class countdown : MonoBehaviour {
+
+    public Image circularSilder;            //Drag the circular image i.e Slider in our case
+    public float time;                      //In how much time the progress bar will fill/empty
+    public GameObject Ofelia;
+
+    void Start()
+    {
+        circularSilder.fillAmount = 10f;      // Initally progress bar is empty
+    }
+    void Update()
+    {
+        if (Ofelia.GetComponentInChildren<CanvasGroup>().alpha == 1)
+        {
+            circularSilder.fillAmount -= Time.deltaTime / time;
+        }
+    }
+}
