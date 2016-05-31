c2d5d37fbabec83f057e69d21b60dca2f41b8f66
Alexis Jauregui <ajaureg2@adgpm.soe.ucsc.edu>
2016-05-30 19:09:22 -0700

    playtest

--- Desi Y Ofelia/Assets/Scripts/DesiScene.cs
+++ Desi Y Ofelia/Assets/Scripts/DesiScene.cs
@@ -14,7 +14,7 @@ public class DesiScene : NetworkBehaviour
     public Text Sign2;
     public Text Sign3;
     public Text Sign4;
-    public GameObject spawn;
+    //public GameObject spawn;
     [SerializeField]
     public bool doorCollision0;
     [SerializeField]
@@ -72,7 +72,7 @@ public class DesiScene : NetworkBehaviour
                 if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("OfeliaPlayer").transform.position) < 4)
                 {
                     Sign0.enabled = false;
-                    spawn.transform.position = new Vector3(-4, 1, -34);
+    
                     NetworkManager.singleton.ServerChangeScene("Level 0");
                 }
             }
@@ -86,7 +86,7 @@ public class DesiScene : NetworkBehaviour
                 if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("OfeliaPlayer").transform.position) < 4)
                 {
                     Sign1.enabled = false;
-                    spawn.transform.position = new Vector3(9, 1, -22);
+                    //spawn.transform.position = new Vector3(9, 1, -22);
                     NetworkManager.singleton.ServerChangeScene("Level 1");
                 }
             }
@@ -100,7 +100,7 @@ public class DesiScene : NetworkBehaviour
                 if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("OfeliaPlayer").transform.position) < 4)
                 {
                     Sign2.enabled = false;
-                    spawn.transform.position = new Vector3(-5, 1, -4);
+                    //spawn.transform.position = new Vector3(-5, 1, -4);
                     NetworkManager.singleton.ServerChangeScene("Level 3");
                 }
             }
